6bb0c5663c00322f1f5b28ee66beb7ba66f3aded
Redeat Ephrem <101739335+Rediii8@users.noreply.github.com>
2026-03-18 22:36:01 +0300

    Add solution for Replace the Substring for Balanced String (trial 1)

---
+++ leetcode/replace-the-substring-for-balanced-string_trial1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def balancedString(self, s: str) -> int:
+        from collections import Counter
+        
+        n = len(s)
+        count = Counter(s)
+        required = n // 4
+        
+        
+        if all(count[c] == required for c in "QWER"):
+            return 0
+        
+        left = 0
+        min_len = n
+        
+        for right in range(n):
+            count[s[right]] -= 1
+            
+            while left < n and all(count[c] <= required for c in "QWER"):
+                min_len = min(min_len, right - left + 1)
+                count[s[left]] += 1
+                left += 1
+        
+        return min_len    
