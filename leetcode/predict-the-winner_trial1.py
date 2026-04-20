a7a9e1cfeef58797d6ab708e59b4e64fa09f96e5
Redeat Ephrem <101739335+Rediii8@users.noreply.github.com>
2026-04-20 21:03:11 +0300

    Add solution for Predict the Winner (trial 1)

---
+++ leetcode/predict-the-winner_trial1.py
@@ -0,0 +1,15 @@
+from functools import lru_cache
+
+class Solution:
+    def predictTheWinner(self, nums):  
+        @lru_cache(None)
+        def dp(l, r):
+            if l == r:
+                return nums[l]
+            
+            return max(
+                nums[l] - dp(l + 1, r),
+                nums[r] - dp(l, r - 1)
+            )
+        
+        return dp(0, len(nums) - 1) >= 0
