5f4988845a7082105cd53e2deaf8ebd842058ecd
Redeat Ephrem <101739335+Rediii8@users.noreply.github.com>
2026-04-28 22:18:31 +0300

    Add solution for Binary Tree Inorder Traversal (trial 1)

---
+++ leetcode/binary-tree-inorder-traversal_trial1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = []
+        result = []
+        current = root
+        
+        while current or stack:
+           
+            while current:
+                stack.append(current)
+                current = current.left
+            
+            current = stack.pop()
+            result.append(current.val)
+            
+           
+            current = current.right
+        
+        return result
