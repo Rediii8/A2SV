26c96cdfcc2d701f7405031e701951b95ad79e31
Redeat Ephrem <101739335+Rediii8@users.noreply.github.com>
2026-04-29 20:22:02 +0300

    Add solution for Binary Tree Postorder Traversal (trial 1)

---
+++ leetcode/binary-tree-postorder-traversal_trial1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def postorderTraversal(self, root):
+        if not root:
+            return []
+        
+        stack = [root]
+        res = []
+        
+        while stack:
+            node = stack.pop()
+            res.append(node.val)
+            
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        
+        return res[::-1]
